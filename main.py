1f01efb5389ccf462ec185fc442c9b578e29af42
Theo Santoro <theoalmeida00@gmail.com>
2026-03-24 16:58:49 -0300

    corrigido problema de lib desatualizadas e incluido requirements.txt

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-from random import choice
 import discord
 from discord.ext import commands
 import yt_dlp as youtube_dl
-from py_yt import VideosSearch
 import os
 from load_dotenv import load_dotenv
 
-'''
-PT-BR:
-    Lembre-se de alterar/criar seu arquivo .env com o caminho da pasta de imagens e application id.
-
-EN-US:
-    Remember to create/change your .env file with informations about image folder path and application id
-'''
-
 load_dotenv()
 
-IMG_FOLDER_PATH = os.getenv('IMG_FOLDER_PATH')
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-if not DISCORD_TOKEN or not IMG_FOLDER_PATH:
-    print('Please make sure you have DISCORD_TOKEN and IMG_FOLDER_PATH on your .env file, also check for the name of the variables')
-    print('Por favor, certifique-se que as variáveis DISCORD_TOKEN e IMG_FOLDER_PATH estao corretamente salvas no .env')
+if not DISCORD_TOKEN:
+    print('Please make sure you have DISCORD_TOKEN on your .env file')
+    print('Por favor, certifique-se que a variável DISCORD_TOKEN está corretamente salva no .env')
     raise Exception
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-
-images_folder = IMG_FOLDER_PATH
 
 # Class to manage music queue
 class MusicQueue:
@@ -59,7 +46,8 @@ async def on_ready():
     # Send message in discord chat when bot is turned on
     for guild in bot.guilds:
         text_channel = guild.text_channels[0]
-        await text_channel.send(file=discord.File(os.path.join(images_folder, 'welcome.png')), content="Rei Macaco na área!")
+        # await text_channel.send(file=discord.File(os.path.join(images_folder, 'welcome.png')), content="Rei Macaco na área!")
+        await text_channel.send(content="Rei Macaco na área!")
 
 @bot.command(name='play', help='Busca e reproduz uma música do YouTube')
 async def play(ctx, *, search):
@@ -70,53 +58,33 @@ async def play(ctx, *, search):
 
     channel = ctx.author.voice.channel
 
-    # Search for the video user entered
-    videos_search = VideosSearch(search, limit=1, region='BR')
-    try:
-        video_result = await videos_search.next()
-        print(video_result['result'][0]['link'])
-        url = video_result['result'][0]['link']
-    except Exception as e:
-        print(f'Erro ao pesquisar musica: {e}')
-        # Images to be sent when bot can't find the music
-        await ctx.send(file=discord.File(os.path.join(images_folder, 'erro.png')), content="Houve um problema ao tentar buscar a música.")
-        return
-
     ydl_opts = {
         'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
+        'quiet': True,
+        'default_search': 'ytsearch',
+        'noplaylist': True,
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
-            info = ydl.extract_info(url, download=False)
-            for format in info['formats']:
-                if format['ext'] == 'm4a':
-                    song = {'source': format['url'], 'title': info['title']}
+            info = ydl.extract_info(f'ytsearch1:{search}', download=False)
+            info = info['entries'][0]
+            for fmt in info['formats']:
+                if fmt['ext'] == 'm4a':
+                    song = {'source': fmt['url'], 'title': info['title']}
                     break
             else:
                 song = {'source': info['formats'][0]['url'], 'title': info['title']}
-            print(f'Playing URL: {song["source"]}')  # Log to verify audio url
+            print(f'Playing URL: {song["source"]}')
             music_queue.add_to_queue(song, channel)
 
-            # Images to be sent when user ask for a music
-            file_to_send = choice(['emoji.png',
-                                   'emoji1.png',
-                                   'emoji2.png',
-                                   'emoji3.png'])
-            
-            await ctx.send(file=discord.File(os.path.join(images_folder, file_to_send)), content=f'Adicionada à fila: {song["title"]}')
+            await ctx.send(content=f'Adicionada à fila: {song["title"]}')
 
             if not music_queue.is_playing:
                 await play_next(ctx)
         except Exception as e:
-            print(f'Erro ao extrair informação: {e}')
-            # Images to be sent when bot can't find the music
-            await ctx.send(file=discord.File(os.path.join(images_folder, 'erro.png')), content="Houve um problema ao tentar reproduzir a música.")
+            print(f'Erro ao buscar/extrair música: {e}')
+            await ctx.send(content="Houve um problema ao tentar buscar a música.")
 
 async def play_next(ctx):
     next_song = music_queue.get_next_song()
@@ -140,10 +108,8 @@ async def play_next(ctx):
 
 @bot.command(name='next', help='Pula a música atual e reproduz a próxima da fila')
 async def next(ctx):
-    # Function to skip music
     if music_queue.vc and music_queue.vc.is_playing():
-        music_queue.vc.stop()
-        await play_next(ctx)
+        music_queue.vc.stop()  # after callback will call play_next automatically
     else:
         await ctx.send("Não há nenhuma música sendo reproduzida no momento.")
 
